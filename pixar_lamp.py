eafd2ac7cb66c7e5bed575723853836d3934a4f8
Lewis Chi <lewis.chi@servicenow.com>
2023-12-22 16:15:49 -0800

    update pixar_lamp info

---
+++ pixar_lamp.py
@@ -0,0 +1,14 @@
+from smart_plug import SmartPlug
+from constants import *
+import asyncio
+
+async def toggle():
+  plug = SmartPlug(PIXAR_LAMP)
+  await plug.update()
+
+  if plug.is_on:
+    await plug.turn_off()
+  elif plug.is_off:
+    await plug.turn_on()
+
+asyncio.run(toggle())
